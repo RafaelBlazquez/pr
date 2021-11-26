daf465dd48eea4c25db3d36d0869059905b04cad
RafaelBlazquez <73467706+RafaelBlazquez@users.noreply.github.com>
2021-11-26 08:27:56 +0100

    Add files via upload

--- 2GDAW_Reto_2-main/Servidor Script/view/basedatos/recientes.php
+++ 2GDAW_Reto_2-main/Servidor Script/view/basedatos/recientes.php
@@ -1,20 +1,20 @@
 <?php
     
     function anadirReciente($reciente){
-        if(!isset($_SESSION['usuario'])){
         session_start();
-        if(!isset($_SESSION['recientes'])){
-            $arrayRecientes= array($reciente);
-        }
-        else{
-            $arrayRecientes= explode(",",$_SESSION['recientes']);
-            if(($posicion = array_search($reciente, $arrayRecientes)) !== false){
-                unset($arrayRecientes[$posicion]);
+        if(isset($_SESSION['usuario'])){
+            if(!isset($_SESSION['recientes'])){
+                $arrayRecientes= array($reciente);
             }
-            array_unshift($arrayRecientes,$reciente);
-        }
-        $stringRecientes = implode(",",$arrayRecientes);
-        $_SESSION['recientes'] =$stringRecientes;
+            else{
+                $arrayRecientes= explode(",",$_SESSION['recientes']);
+                if(($posicion = array_search($reciente, $arrayRecientes)) !== false){
+                    unset($arrayRecientes[$posicion]);
+                }
+                array_unshift($arrayRecientes,$reciente);
+            }
+            $stringRecientes = implode(",",$arrayRecientes);
+               $_SESSION['recientes'] = $stringRecientes;
         }
     }
 ?>
